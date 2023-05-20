6cd90c7b23d707fff1b989faeb7e60e47c0231c9
Srix <neosrix@gmail.com>
2023-05-20 19:16:35 +0530

    updated highlight api call as per this [200~https://github.com/highlightjs/highlight.js/issues/2277~

--- docs/script.js
+++ docs/script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     marked.setOptions({
         highlight: function(code, lang) {
           if (lang && hljs.getLanguage(lang)) {
-            return hljs.highlight(lang, code).value;
+            return hljs.highlight( code,  {language: lang}).value;
           } else {
             return hljs.highlightAuto(code).value;
           }
